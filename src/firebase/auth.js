a22e2fce2b16f0c0498695c6d8d2f7f0009d7c54
Thais Alves Pereira <tathyflor@gmail.com>
2023-07-05 13:11:50 -0300

    alterações no auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,75 +1,88 @@
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, updateProfile } from 'firebase/auth';
+import {
+  getAuth,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  updateProfile,
+  signOut,
+} from 'firebase/auth';
 import { app } from './firebase.js';
 
 // Inicialize o módulo de autenticação do Firebase
 const auth = getAuth(app);
 
-// Função para criar um novo usuário com e-mail e senha
-export const createUserWithEmail = async (email, password, displayName) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    await updateProfile(user, { displayName });
-    console.log('Usuário criado:', user);
-  } catch (error) {
-    console.log('Erro ao criar usuário:', error);
-  }
-};
+// criar usuário com email e senha
+export const signUp = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+// criar promessa e callback caso o cadastro dê erro, e o updatProfile
 
+// usuário entra com email e senha
+export const signIn = (email, password) => signInWithEmailAndPassword(auth, email, password);
+// callback e promessa, ir para o feed
+
+// entrar e cadastrar com google - verificar se está funcionando quando arrumar a pagina de login
 export const signInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
-  return signInWithPopup(auth, provider)
-    .then((userCredential) => {
-      const user = userCredential.user;
+  return signInWithRedirect(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      console.log(token);
+      const user = result.user;
+      if (credential) {
+        if (user) {
+          console.log('novo usuário criado');
+        } else {
+          console.log('usuário existente');
+        }
+      }
       console.log('Usuário logado:', user);
       // Redirecionar para a página desejada
       window.location.hash = '#feed';
     })
     .catch((error) => {
       console.log('Erro de login com o Google:', error);
-      alert('Erro ao fazer login com o Google. Verifique suas credenciais e tente novamente.');
+      console.log('Erro ao fazer login com o Google. Verifique suas credenciais e tente novamente.');
     });
 };
 
+// entrar e cadastrar com o facebook - verificar se está funcionando quando arrumar a page login
 export const signInWithFacebook = async () => {
   const provider = new FacebookAuthProvider();
-  return signInWithPopup(auth, provider)
-    .then((userCredential) => {
-      const user = userCredential.user;
+  return signInWithRedirect(auth, provider)
+    .then((result) => {
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      console.log(token);
+      const user = result.user;
+      if (credential) {
+        if (user) {
+          console.log('novo usuário criado');
+        } else {
+          console.log('usuário existente');
+        }
+      }
       console.log('Usuário logado:', user);
       // Redirecionar para a página desejada
       window.location.hash = '#feed';
     })
     .catch((error) => {
       console.log('Erro de login com o Facebook:', error);
-      alert('Erro ao fazer login com o Facebook. Verifique suas credenciais e tente novamente.');
+      console.log('Erro ao fazer login com o Facebook. Verifique suas credenciais e tente novamente.');
     });
 };
 
-// Função para realizar o login com email e senha
-export const loginWithEmail = async (email, password) => {
-  try {
-    await firebase.auth().signInWithEmailAndPassword(email, password);
-    // O login foi realizado com sucesso
-  } catch (error) {
-    throw error;
-  }
-};
-
 // Função para realizar o logout
-export const logout = async () => {
-  try {
-    await firebase.auth().signOut();
-    // O logout foi realizado com sucesso
-  } catch (error) {
-    throw error;
-  }
-};
+export const logout = () => signOut(auth);
+
+
+// falta esses abaixo
 
 // Função para verificar se um usuário está logado
 export const checkLoggedUser = () => {
   const auth = getAuth();
-
   return new Promise((resolve, reject) => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -84,17 +97,15 @@ export const checkLoggedUser = () => {
 };
 
 // Obtém o usuário atualmente autenticado
-export const getCurrentUser = () => {
-  return new Promise((resolve, reject) => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        resolve(user);
-      } else {
-        reject(new Error('Nenhum usuário autenticado.'));
-      }
-    });
+export const getCurrentUser = () => new Promise((resolve, reject) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      resolve(user);
+    } else {
+      reject(new Error('Nenhum usuário autenticado.'));
+    }
   });
-};
+});
 
 // Função para atualizar a senha do usuário atualmente logado
 export const updatePassword = async (newPassword) => {
